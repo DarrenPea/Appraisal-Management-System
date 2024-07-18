92da79ed58e7b723a0f8e971f816d136564de4ab
wanqing <118462447+wanq27@users.noreply.github.com>
2024-07-19 03:12:46 +0800

    Update server_clock.js

--- tasks/server_clock.js
+++ tasks/server_clock.js
@@ -1,18 +1,15 @@
-const cron = require('node-cron');
 const employeeModel = require('../models/employee.js');
 const formModel = require('../models/appraisalform.js');
 
-
-// cron.schedule(minute, hour, day of month, month, day of week)
-// clock will schedule on the first day of every month to assign appraisal to employee 
-// if the current month is the month of their next appraisal date
+// function to update next appraisal date for employee and to create appraisal form for employee
+// if the current month is the month of the employee's next appraisal date
 
 async function checkForAppraisal() {
     try {
         const employees = await employeeModel.findByAppraisalDateDue();
         for (const employee of employees) {
             const {employeeID, managerID} = employee;
-            await employeeModel.updateNextAppraisalDate(employeeID);
+            await employeeModel.updateNextAppraisalDate()
             await formModel.createEntry(employeeID, managerID);
         }
     } catch(error) {
@@ -20,4 +17,9 @@ async function checkForAppraisal() {
     }
 }
 
-cron.schedule('0 0 1 * *', checkForAppraisal);
+// Initial call to perform the task immediately upon starting
+checkForAppraisal();
+
+module.exports = {
+    checkForAppraisal // Export the function to be used in www.js
+};
