2bd644f15388006f7063f853ad2b218ec64d9491
jiasing <67702414+jiasing@users.noreply.github.com>
2024-07-19 01:26:24 +0800

    Updated code for appraisal form models & routes
    made a few changes to the routes / functions

--- models/appraisalform.js
+++ models/appraisalform.js
@@ -2,13 +2,15 @@ const db = require('./db.js');
 const tableName = 'appraisalform';
 const employeetable = 'employee';
 
+//i think i didnt even use this appraisalform class
 class AppraisalForm {
   constructor() {
     this.formID = null;
     this.employeeID = null;
     this.statusEmployee = null;
     this.statusHOD = null;
-    this.formDateUploaded = null;
+    this.dateCreated = null;
+    this.lastUpdated = null;
     this.a1 = null;
     this.a2_1 = null;
     this.a2_2 = null;
@@ -71,7 +73,8 @@ async function sync() {
         managerID INT,
         statusEmployee BOOLEAN,
         statusHOD BOOLEAN,
-        formDateUploaded DATETIME,
+        dateCreated DATETIME,
+        lastUpdated DATETIME,
         a1 VARCHAR(255),
         a2_1 VARCHAR(255),
         a2_2 VARCHAR(255),
@@ -130,93 +133,52 @@ async function sync() {
   }
 }
 
-// /form/retrieve. NEED TO DELETE?
-async function blank_form() {
-    return new AppraisalForm();
-}
-
-//appraisalform is an AppraisalForm object with the fields filled up accordingly.
-// /form/employee/submit
-async function insertOne(employeeID, formfields) {
-  if (formfields.length != 50) {
-    throw new Error("insertOne: something wrong with formfields length, it is " + formfields.length);
-  }
-  
-  let managerID;
-  // Query for managerID
+// /form/employee/retrieve
+async function retrieve(employeeID) {
   try {
-    const query = `SELECT managerID FROM ${employeetable} WHERE employeeID = ?`;
-    const result = await db.query(query, [employeeID]);
-    if (result.length > 0) {
-      managerID = result[0].managerID;
-    } else {
-      throw new Error("insertOne: Can't obtain managerID with employeeID:" + employeeID); // No matching employee found
-    }
+    const curr_year = new Date().getFullYear();
+    const curr_month = new Date().getMonth() + 1; // JavaScript months are 0-indexed, SQL months are 1-indexed
+    const query = `SELECT formID FROM ${tableName} WHERE employeeID = ? AND YEAR(dateCreated) = ? AND MONTH(dateCreated) = ?`;
+    const [rows] = await db.pool.query(query, [employeeID, curr_year, curr_month]);
+    return rows;  
   } catch (error) {
-    console.error("Error fetching manager ID:", error);
-    throw error; // Rethrow or handle as needed
-  }
-
-  // Updating appraisalForm table
-  try {
-    const query = `
-      INSERT INTO ${tableName} (
-        employeeID, managerID, statusEmployee, statusHOD, formDateUploaded, 
-        a1, a2_1, a2_2, a2_3, a2_4, a2_5, a2_6, a2_7, a2_8, 
-        a3_1, commentsA3_1, a3_2, commentsA3_2, a3_3, commentsA3_3, 
-        a3_4, commentsA3_4, a3_5, commentsA3_5, a3_6, commentsA3_6, 
-        a3_7, commentsA3_7, a3_8, commentsA3_8, a3_9, commentsA3_9, 
-        a3_10, commentsA3_10, a3_11, commentsA3_11, a3_12, commentsA3_12, 
-        a3_13, commentsA3_13, a3_14, commentsA3_14, a3_15, commentsA3_15, 
-        a3_16, commentsA3_16, a3_17, commentsA3_17, a3_18, commentsA3_18, 
-        a3_19, commentsA3_19, a3_20, commentsA3_20, b
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-    const values = [employeeID, managerID, true, false, new Date(), ...formfields];
-    await db.pool.query(query, values);
-    return true;
-  } catch (error) {
-    console.error("Database connection failed. Problem: insertOne(appraisalForm)! " + error);
-    throw error; // Rethrow or handle as needed
+    console.error("Error fetching form: ", error);
+    throw error;
   }
 }
 
 
 //retrieveForm returns ONE SINGULAR FORM to be FILLED UP
-async function retrieveForm(employeeID){
+async function retrieveForm(formID){
   try{
-    const curr_year = new Date().getFullYear();
     const query = `
       SELECT * 
       FROM ${tableName} 
-      WHERE employeeID = ? 
-        AND YEAR(FormDateUploaded) = ?
+      WHERE formID = ? 
     `;
 
     //rows is an array of data
-    const [rows] = await db.pool.query(query, [employeeID, curr_year]);
+    const [rows] = await db.pool.query(query, [formID]);
 
-    // slice 5 cause we only want to give the columns pertaining to the questions in the form
     return rows
   } 
   catch (error) {
     throw new Error('Form not retrieved');
   }
-}
-
+} 
 
 // /form/HOD/submit
-async function updateOne(formID, formfields) {
-  if (formfields.length != 52) {
-    throw new Error("updateOne: formfields length is not 52, it is " + formfields.length);
+async function updateOne(formID, formfields, statusHOD = false) {
+  if (formfields.length != 50) {
+    throw new Error("updateOne: formfields length is not 50, it is " + formfields.length);
   }
-  const curr_year = new Date().getFullYear();
 
   try {
+    
     // we dont insert into formID cause that'll be auto generated
     const query = `
     UPDATE ${tableName} SET
-    statusHOD = ?, formDateUploaded = ?, 
+    statusEmployee = ?, statusHOD = ?, lastUpdated = ?, 
     a1 = ?, a2_1 = ?, a2_2 = ?, a2_3 = ?, a2_4 = ?, a2_5 = ?, a2_6 = ?, a2_7 = ?, a2_8 = ?, 
     a3_1 = ?, commentsA3_1 = ?, a3_2 = ?, commentsA3_2 = ?, a3_3 = ?, commentsA3_3 = ?, 
     a3_4 = ?, commentsA3_4 = ?, a3_5 = ?, commentsA3_5 = ?, a3_6 = ?, commentsA3_6 = ?, 
@@ -225,10 +187,10 @@ async function updateOne(formID, formfields) {
     a3_13 = ?, commentsA3_13 = ?, a3_14 = ?, commentsA3_14 = ?, a3_15 = ?, commentsA3_15 = ?, 
     a3_16 = ?, commentsA3_16 = ?, a3_17 = ?, commentsA3_17 = ?, a3_18 = ?, commentsA3_18 = ?, 
     a3_19 = ?, commentsA3_19 = ?, a3_20 = ?, commentsA3_20 = ?, b = ?
-    WHERE formID = ? AND YEAR(FormDateUploaded) = ?
+    WHERE formID = ? 
     `;
     // Add formID and current year to the values array
-    const values = [...formfields, formID, curr_year]
+    const values = [true, statusHOD, new Date(),formfields, formID]
 
     await db.pool.query(query, values);
     return true;
@@ -241,24 +203,46 @@ async function updateOne(formID, formfields) {
 
 // /form/HOD/status
 // returns an array where each element is an array that represents a form
-async function retrieveAllForms(managerID){
+async function retrieveForHOD(managerID){
   const curr_year = new Date().getFullYear();
+  const curr_month = new Date().getMonth() + 1; // JavaScript months are 0-indexed, SQL months are 1-indexed
+
   try{
     // get table entries based on managerID and current year.
     const query = `
-      SELECT EmployeeID, ManagerID, StatusEmployee, StatusHOD, FormDateUploaded 
+      SELECT formID, employeeID, managerID, statusEmployee, statusHOD, lastUpdated 
       FROM ${tableName}
-      WHERE ManagerID = ?
-      AND YEAR(FormDateUploaded) = ?
+      WHERE managerID = ?
+      AND YEAR(dateCreated) = ? AND MONTH(dateCreated) = ?
 
     `;
 
     //rows is an array of data. 
-    const [rows] = await db.pool.query(query, [managerID, curr_year]);
+    const [rows] = await db.pool.query(query, [managerID, curr_year, curr_month]);
     return rows
 
   } 
   catch (error) {
     throw new Error('Form not retrieved, problem with retrieveAllForms');
+  }
+}
+
+// /form/HR/status
+async function retrieveForHR(){
+  try{
+    const curr_year = new Date().getFullYear();
+    const curr_month = new Date().getMonth() + 1; // JavaScript months are 0-indexed, SQL months are 1-indexed
+    const query = `
+      SELECT formID, employeeID, managerID, statusEmployee, statusHOD, lastUpdated 
+      FROM ${tableName}
+      WHERE YEAR(FormDateUploaded) = ? AND MONTH(FormDateUploaded) = ?
+    `;
+
+    //rows is an array of data
+    const [rows] = await db.pool.query(query, [curr_year, curr_month]);
+    return rows
+  } 
+  catch (error) {
+    throw new Error('Form not retrieved, problem with retrieveForHR');
   }
 }
--- routes/appraisalform.js
+++ routes/appraisalform.js
@@ -2,19 +2,6 @@ const express = require("express");
 const formModel = require("../models/appraisalform.js");
 var router = express.Router();
 
-// GET: /form/retrieve
-// Use this to pull a NEW appraisal form for the employee to fill up.
-router.get("/retrieve/", async (req, res) => {
-    try {
-        const formInstance = formModel.blank_form();
-        res.json(formInstance); // Send the data as JSON
-    } catch (error) {
-        console.error("Error fetching form data:", error);
-        res.status(500).send("Server error");
-    }
-  });
-
-
 //   Submit the fully filled up appraisal form by employee.
 //   FE send: completed form (by employee only), staffID
 //   {staffID: val1, {qn1: None, qn2: None,..., last_qn: None}}
@@ -24,13 +11,25 @@ router.post("/employee/submit", async(req,res) => {
         const list_of_data = req.body;
         const staffID = list_of_data[0];
         const formfields = list_of_data.slice(1,);
-        const upload_status = await formModel.insertone(staffID, formfields);
+        const upload_status = await formModel.updateOne(staffID, formfields);
         res.send(JSON.stringify(upload_status));
     }catch(error){
         console.error(error);
         res.status(404).send(JSON.stringify({ message: 'problem with /form/employee' }));
     }
 });
+
+// retrieve form for employee to fill up
+router.post("/employee/retrieve", async(req,res) => {
+    try{
+        const staffID = req.body;
+        const form = await formModel.retrieveform(staffID);
+        res.send(JSON.stringify(form));
+    }catch(error){
+        console.error(error);
+        res.status(404).send(JSON.stringify({ message: 'problem with /form/employee/retrieve' }));
+    }
+})
 
 // use by HOD to submit their completed appraisal form to backend for storage.
 // FE send: formID, completed form (by HOD)
@@ -41,9 +40,7 @@ router.post("/HOD/submit", async(req,res) => {
         const list_of_data = req.body;
         const formID = list_of_data[0];
         const formfields = list_of_data.slice(1,);
-        // we only update status_HOD, latest upload and ALL of form fields.
-        const to_upload = [true, new Date(), ... formfields];
-        const upload_status = await formModel.updateone(formID, to_upload);
+        const upload_status = await formModel.updateone(formID, formfields, true);
         res.send(JSON.stringify(upload_status));
     }catch(error){
         console.error(error);
@@ -59,8 +56,8 @@ router.post("/HOD/submit", async(req,res) => {
 // *FE need to take note which questions can be filled up by HOD.
 router.post("/HOD/retrieve", async(req,res) => {
     try{
-        const employeeID = req.body;
-        const form = await formModel.retrieveform(employeeID);
+        const formID = req.body;
+        const form = await formModel.retrieveform(formID);
         res.send(JSON.stringify(form));
     }catch(error){
         console.error(error);
@@ -75,8 +72,8 @@ router.post("/HOD/retrieve", async(req,res) => {
 // {qn1: val1, qn2: val2,...,last_qn:valLast}
 router.post("/completed", async(req,res) => {
     try{
-        const employeeID = req.body;
-        const form = await formModel.retrieveform(employeeID);
+        const formID = req.body;
+        const form = await formModel.retrieveform(formID);
         res.send(JSON.stringify(form));
     }catch(error){
         console.error(error);
@@ -93,12 +90,24 @@ router.post("/completed", async(req,res) => {
 router.post("/HOD/status", async(req,res) => { 
     try{
     const HOD_ID = req.body;
-    const form = await formModel.retrieveAllForms(HOD_ID);
+    const form = await formModel.retrieveForHOD(HOD_ID);
     res.send(JSON.stringify(form));
     }catch(error){
         console.error(error);
         res.status(404).send(JSON.stringify({ message: 'problem with /form/completed' }));
     }
 });
+
+// 9.) POST: /form/HR/status
+// use by HR to retrieve and view the list of all forms for the month.
+router.get("/HR/status", async(req,res) => {
+    try{
+        const forms = await formModel.retrieveForHR();
+        res.send(JSON.stringify(forms));
+    }catch(error){ 
+        console.error(error);
+        res.status(404).send(JSON.stringify({ message: 'problem with /form/HR/status' }));
+    }
+})
 
 module.exports = router;
